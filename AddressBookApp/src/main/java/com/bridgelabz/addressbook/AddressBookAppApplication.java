e3a2684c4e64cd331baa9c5c295559af67635911
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-10 08:44:53 +0530

    ADDED: Ability to REST request from addressbook UI

--- AddressBookApp/src/main/java/com/bridgelabz/addressbook/AddressBookAppApplication.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbook/AddressBookAppApplication.java
@@ -2,12 +2,14 @@ package com.bridgelabz.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+		System.out.println(context);
 	}
 
 }
