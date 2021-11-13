db2aa7f541865eeacfc1d5bd35e7173b98227a44
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-13 08:52:10 +0530

    ADDED: ability validated name and zip

--- AddressBookApp/src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
@@ -1,15 +1,21 @@
 package com.bridgelabz.addressbook.dto;
 
+import javax.validation.constraints.Pattern;
+
 import lombok.ToString;
 
 @ToString
 public class ContactDTO {
+	@Pattern(regexp = "^[A-Z]{1,}[a-zA-z\\s]{2,}$", message = "FirstName is invalid")
 	public String firstName;
+	@Pattern(regexp = "^[A-Z]{1,}[a-zA-z\\s]{2,}$", message = "LastName is invalid")
 	public String lastName;
 	public String address;
 	public String state;
 	public String city;
+	@Pattern(regexp = "^[0-9]{3}\\s{0,1}[0-9]{3}$", message = "Invalid Zip")
 	public String zip;
+	@Pattern(regexp = "^^[0-9]{2}?[\\s,-]{0,1}[7-9]{1}[0-9]{9}$", message = "Invalid Phone number")
 	public String phone;
 
 	public ContactDTO(String firstName, String lastName, String address, String state, String city, String zip,
