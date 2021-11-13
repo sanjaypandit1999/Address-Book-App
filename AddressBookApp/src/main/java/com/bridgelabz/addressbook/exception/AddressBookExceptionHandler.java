6939607a1a45534a93890831c8eb16e51142d14b
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-13 09:11:02 +0530

    ADDED: ability to throw custom exception

--- AddressBookApp/src/main/java/com/bridgelabz/addressbook/exception/AddressBookExceptionHandler.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbook/exception/AddressBookExceptionHandler.java
@@ -15,14 +15,20 @@ import com.bridgelabz.addressbook.dto.ResponseDTO;
 @ControllerAdvice
 public class AddressBookExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception) {
-        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
-        List<String> errorMessage = errorList.stream().map(objerr -> objerr.getDefaultMessage())
-                .collect(Collectors.toList());
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errorMessage = errorList.stream().map(objerr -> objerr.getDefaultMessage())
+				.collect(Collectors.toList());
 
-        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errorMessage);
-        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
-    }
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errorMessage);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(AddressBookException.class)
+	public ResponseEntity<ResponseDTO> handleEmployeePayrollException(AddressBookException exception) {
+		ResponseDTO responseDTO = new ResponseDTO("Exception while REST Request", exception.getMessage());
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
 }
