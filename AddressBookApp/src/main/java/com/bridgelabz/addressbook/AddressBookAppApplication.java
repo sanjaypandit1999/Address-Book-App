abcdec99e2f4f4e48272cb2463c5990055bf44cf
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-13 09:44:52 +0530

    ADDED: Aplicatin properties for dev and prod

--- AddressBookApp/src/main/java/com/bridgelabz/addressbook/AddressBookAppApplication.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbook/AddressBookAppApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
 		System.out.println(context);
+		log.info("AddressBook App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
 	}
 
 }
